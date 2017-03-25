5b29d6e9225305e266375a7968efb57e84cec17f
Euibin Cheon <euibinc@gmail.com>
2017-03-25 03:55:21 -0400

    Update .gitignore and initialize npm package

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+  #
+  # namespace :api, defaults: {format: :json} do
+  #   resources :users, only: [:create, :show]
+  #   resources :events, only: [:index, :create, :destroy, :update, :show]
+  #   resources :bookmarks, only: [:index, :create, :destroy]
+  #   resources :tickets, only: [:index, :create, :destroy]
+  #   resources :types, only: :index
+  #   resources :categories, only: :index
+  #
+  #   resource :session, only: [:create, :destroy]
+  # end
 end
