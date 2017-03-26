f06a060de8e28b26d47a9146af35c99031437b6d
Euibin Cheon <euibinc@gmail.com>
2017-03-26 02:14:10 -0400

    Add figaro and hide keys

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -1,12 +1,12 @@
 class Api::TweetsController < ApplicationController
   def search
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "Dl0EU13B6ecJxDyiDl0NXNGkW"
-      config.consumer_secret     = "xixpTPbV1BlMWWO8jhx4nO3sxYoloQDjlkdB1x2Sq6ISXOJ4qx"
-      config.access_token        = "845455556500471808-9YsN2Lmh7MskuYZNJ46RAiweDrhoA6G"
-      config.access_token_secret = "WsmMCc8Sw1kNIzKqvktzUSk6044uL9kygRaBTDbvs1Oxu"
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['YOUR_ACCESS_TOKEN']
+      config.access_token_secret = ENV['YOUR_ACCESS_SECRET']
     end
-    
+
     result = client.search(search_query, search_option)
 
     render json: result
