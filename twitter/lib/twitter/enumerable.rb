dd01f493adbdde2d4016beabef6ea0d7e3266ceb
Euibin Cheon <euibinc@gmail.com>
2017-05-16 21:54:33 -0400

    Modify twitter API

---
+++ twitter/lib/twitter/enumerable.rb
@@ -0,0 +1,31 @@
+module Twitter
+  module Enumerable
+    include ::Enumerable
+
+    # @return [Enumerator]
+    def each(start = 0)
+      @page_record ||= 0
+      return to_enum(:each, start) unless block_given?
+      # Array(@collection[start..-1]).each do |element|
+      slice_index = @page_record >= start ? 0 : start - @page_record
+      Array(@collection[slice_index..-1]).each do |element|
+        yield(element)
+      end
+      unless last?
+        # start = [@collection.size, start].max
+        @page_record += @collection.size
+        @collection = []
+        fetch_next_page
+        each(start, &Proc.new)
+      end
+      self
+    end
+
+  private
+
+    # @return [Boolean]
+    def last?
+      true
+    end
+  end
+end
